b5b2da0e4a0990bc6e8182c1553799eab477c8c5
geromegrignon <gerome.grignon.lp2@gmail.com>
2021-07-12 13:32:59 +0200

    refactor: remove starter boilerplate

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -15,17 +15,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'supabase-angular-user-management'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('supabase-angular-user-management');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('supabase-angular-user-management app is running!');
-  });
 });
